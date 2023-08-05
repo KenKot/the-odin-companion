c0a557c1c008ac8100ce9687cef33322017a6ba7
KenKot <kennykotten@gmail.com>
2023-08-04 17:20:22 -0700

    create flashcardJSONData

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,6 +7,8 @@ import Flashcard from "@/models/flashcard";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 
+import flashcardJSONData from "@/misc/flashcardData";
+
 const authOptions = {
   providers: [
     GithubProvider({
@@ -30,41 +32,41 @@ const authOptions = {
           await connectMongoDB();
           let userDoc = await User.findOne({ email });
 
-          let lessonCounter = 0; // Initialize a lesson counter
-
           if (!userDoc) {
-            userDoc = await User.create({ name, email }); // Directly create the user using MongoDB
+            userDoc = await User.create({ name, email });
 
-            // Create two courses for the new user
-            const courses = Array.from({ length: 2 }, async (_, i) => {
-              const course = new Course({ title: `Course ${i + 1}` });
+            // Loop through the courses in your JSON data
+            const courses = flashcardJSONData.courses.map(
+              async (courseData) => {
+                const course = new Course({ title: courseData.title });
 
-              // Create two lessons for each course
-              const lessons = Array.from({ length: 2 }, async (_, j) => {
-                lessonCounter += 1; // Increment the lesson counter
-                const lesson = new Lesson({ title: `Lesson ${lessonCounter}` });
+                // Loop through the lessons in this course
+                const lessons = courseData.lessons.map(async (lessonData) => {
+                  const lesson = new Lesson({ title: lessonData.title });
 
-                // Create three flashcards for each lesson
-                const flashcards = Array.from({ length: 3 }, async (_, k) => {
-                  const flashcard = new Flashcard({
-                    question: `Question ${k + 1}`,
-                    answer: `Answer ${k + 1}`,
-                    isMastered: false,
-                  });
+                  // Loop through the flashcards in this lesson
+                  const flashcards = lessonData.flashcards.map(
+                    async (flashcardData) => {
+                      const flashcard = new Flashcard({
+                        question: flashcardData.question,
+                        answer: flashcardData.answer,
+                      });
 
-                  await flashcard.save();
-                  lesson.flashcards.push(flashcard._id);
+                      await flashcard.save();
+                      lesson.flashcards.push(flashcard._id);
+                    }
+                  );
+
+                  await Promise.all(flashcards);
+                  await lesson.save();
+                  course.lessons.push(lesson._id);
                 });
 
-                await Promise.all(flashcards);
-                await lesson.save();
-                course.lessons.push(lesson._id);
-              });
-
-              await Promise.all(lessons);
-              await course.save();
-              return course._id;
-            });
+                await Promise.all(lessons);
+                await course.save();
+                return course._id;
+              }
+            );
 
             userDoc.courses = await Promise.all(courses);
             await userDoc.save();
