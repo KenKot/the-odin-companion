cda3912fa707ed24d52c9a75a39fa5fdba65c3c0
KenKot <kennykotten@gmail.com>
2023-08-08 02:24:52 -0700

    flashcard.js CSS tweaks

--- components/Flashcard.js
+++ components/Flashcard.js
@@ -9,22 +9,26 @@ export default function Flashcard({ flashcard, toggleFlashcardMastered }) {
   }, [flashcard]);
 
   return (
-    <div
-      className="border-2 border-black m-4 p-4 cursor-pointer flex flex-col items-center max-w-screen-md mx-auto w-full h-[500px] md:w-[800px] md:h-[600px] overflow-auto"
-      onClick={() => {
-        setContentToShow((prevContent) =>
-          prevContent === "question" ? "answer" : "question"
-        );
-      }}
-    >
-      <h2 className="text-xl mb-2">
-        {contentToShow === "question" ? flashcard.question : flashcard.answer}
-      </h2>
+    <div className="border-2 border-black m-4 p-4 cursor-pointer flex flex-col items-center mx-auto h-[500px] w-[400px] md:w-[800px] md:h-[600px]">
+      <div
+        className="overflow-auto flex-grow w-full"
+        onClick={() => {
+          setContentToShow((prevContent) =>
+            prevContent === "question" ? "answer" : "question"
+          );
+        }}
+      >
+        <h2 className="text-xl mb-2 text-center break-words">
+          {contentToShow === "question" ? flashcard.question : flashcard.answer}
+        </h2>
 
-      <p className="mb-2">Is Mastered: {flashcard.isMastered ? "Yes" : "No"}</p>
+        <p className="mb-2 text-center">
+          Is Mastered: {flashcard.isMastered ? "Yes" : "No"}
+        </p>
+      </div>
 
       <button
-        className="border-2 border-red-500 p-2"
+        className="border-2 border-red-500 p-2 mt-4"
         onClick={(e) => {
           e.stopPropagation(); // Prevents the entire div's click event from firing
           toggleFlashcardMastered(flashcard._id, flashcard.isMastered);
@@ -34,63 +38,4 @@ export default function Flashcard({ flashcard, toggleFlashcardMastered }) {
       </button>
     </div>
   );
-
-  //   return (
-  //     <div
-  //       className="border-2 border-black m-2 p-2 cursor-pointer"
-  //       onClick={() => {
-  //         setContentToShow((prevContent) =>
-  //           prevContent === "question" ? "answer" : "question"
-  //         );
-  //       }}
-  //     >
-  //       <h2>
-  //         {contentToShow === "question" ? flashcard.question : flashcard.answer}
-  //       </h2>
-
-  //       <p>Is Mastered: {flashcard.isMastered ? "Yes" : "No"}</p>
-
-  //       <button
-  //         className="border-2 border-red-500"
-  //         onClick={(e) => {
-  //           e.stopPropagation(); // Prevents the entire div's click event from firing
-  //           toggleFlashcardMastered(flashcard._id, flashcard.isMastered);
-  //         }}
-  //       >
-  //         Question Mastered
-  //       </button>
-  //     </div>
-  //   );
 }
-
-// import { useState } from "react";
-
-// export default function Flashcard({ flashcard, toggleFlashcardMastered }) {
-//   const [showAnswer, setShowAnswer] = useState(false);
-
-//   return (
-//     <div className="border-2 border-black m-2 p-2">
-//       <h2>{flashcard.question}</h2>
-
-//       {showAnswer && <h2>{flashcard.answer}</h2>}
-
-//       <p>Is Mastered: {flashcard.isMastered ? "Yes" : "No"}</p>
-
-//       <button
-//         className="border-2 border-red-500"
-//         onClick={() =>
-//           toggleFlashcardMastered(flashcard._id, flashcard.isMastered)
-//         }
-//       >
-//         Question Mastered
-//       </button>
-
-//       <button
-//         className="border-2 border-blue-500 ml-2"
-//         onClick={() => setShowAnswer(!showAnswer)}
-//       >
-//         {showAnswer ? "Hide Answer" : "Show Answer"}
-//       </button>
-//     </div>
-//   );
-// }
--- components/FlashcardViewer.js
+++ components/FlashcardViewer.js
@@ -37,34 +37,4 @@ export default function FlashcardViewer({
       </div>
     </div>
   );
-
-  //   return (
-  //     <div>
-  //       <Flashcard
-  //         flashcard={flashcards[currentIndex]}
-  //         toggleFlashcardMastered={toggleFlashcardMastered}
-  //       />
-
-  //       <div>
-  //         <button
-  //           onClick={() => {
-  //             if (currentIndex > 0) setCurrentIndex((prev) => prev - 1);
-  //           }}
-  //         >
-  //           Previous
-  //         </button>
-  //         <span className="mx-4">
-  //           {currentIndex + 1}/{flashcards.length}
-  //         </span>
-  //         <button
-  //           onClick={() => {
-  //             if (currentIndex < flashcards.length - 1)
-  //               setCurrentIndex((prev) => prev + 1);
-  //           }}
-  //         >
-  //           Next
-  //         </button>
-  //       </div>
-  //     </div>
-  //   );
 }
