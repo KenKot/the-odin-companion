642799885f264c8fbf8ace0997cef04576d31acd
KenKot <kennykotten@gmail.com>
2023-08-16 13:58:05 -0700

    convert page.js into client component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,26 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Courses from "@/components/Courses";
 import Welcome from "@/components/Welcome";
 
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../app/api/auth/[...nextauth]/route";
+export default function Home() {
+  const { status } = useSession();
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  return <>{session ? <Courses /> : <Welcome />}</>;
+  return status === "authenticated" ? <Courses /> : <Welcome />;
 }
+
+// SERVER COMPONENT VERSION BELOW
+
+// import Courses from "@/components/Courses";
+// import Welcome from "@/components/Welcome";
+
+// import { getServerSession } from "next-auth/next";
+// import { authOptions } from "../app/api/auth/[...nextauth]/route";
+
+// export default async function Home() {
+//   const session = await getServerSession(authOptions);
+
+//   return <>{session ? <Courses /> : <Welcome />}</>;
+
+// }
