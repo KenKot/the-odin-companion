71cd2020d891a93e08b3048d77f1b80ea609e154
KenKot <kennykotten@gmail.com>
2023-08-16 15:03:31 -0700

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { NextAuthProvider } from "./Providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
             {children}
           </div>
         </NextAuthProvider>
+        <Analytics />
       </body>
     </html>
   );
